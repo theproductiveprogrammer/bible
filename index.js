8291de3246e8adcfc35eae1c0e803d9c07450c16
Productive Programmer <charles.lobo@gmail.com>
2019-08-03 10:25:07 +1000

    feat: http server

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 #!/usr/bin/env node
 'use strict'
+const express = require('express')
+
 const main = require('./main')
 
-main()
+const app = express()
+
+const PORT = 1923
+
+function httpserver(books) {
+    app.get('/', (req, res) => {
+        sendChapter(main.randomChapter(books), res)
+    })
+
+    app.listen(PORT, (err) => {
+        if(err) console.error(err)
+        else console.log(`Started on port ${PORT}...`)
+    })
+
+}
+
+function sendChapter(book, res) {
+    res.header('Content-Type', 'text/html')
+
+    let testament = 'the old testament'
+    if(book.testament == 'new') testament = 'the new testament'
+    res.write(`<b>From a book of ${testament}: ${book.title}</b>`)
+    res.write(`<br/><pre>(${main.chapterId(book)})</pre>`)
+    res.write('<hr/><pre>')
+    for(let i = 0;i < book.chapter.txt.length;i++) {
+        res.write('<p>')
+        res.write(book.chapter.txt[i])
+        res.write('</p>')
+    }
+    res.write('</pre>')
+
+    res.end()
+}
+
+main.serve(httpserver)
